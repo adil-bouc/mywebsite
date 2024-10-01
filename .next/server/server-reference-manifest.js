7c1c94cdfc04ed58ccab4bf167b35c63569aa483
Adil-bou <adilbouchayoua@gmail.com>
2024-10-01 03:38:34 +0200

    d

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"UrRTPNGpDKPZyXcVGV+CHjI49HhfhCrPtmN0midqbBc=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"d2NkF09aAle6i4P6WqChwuAHVqJgrVphbCyNTuDOfnU=\"\n}"
